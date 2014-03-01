13a4dce8175b946190848762ac8d4c10b37820ae
Ian Grant Jeffries <ian@housejeffries.com>
2014-02-28 22:03:25 -0500

    Handle the exclusiveMinimum schema keyword.

--- validators.go
+++ validators.go
@@ -29,6 +29,7 @@ func (p properties) Validate(v interface{}) []ValidationError {
 
 type minimum struct {
 	json.Number
+	exclusive bool
 }
 
 func (m minimum) Validate(unnormalized interface{}) []ValidationError {
@@ -36,52 +37,69 @@ func (m minimum) Validate(unnormalized interface{}) []ValidationError {
 	if err != nil {
 		return []ValidationError{ValidationError{err.Error()}}
 	}
-	var isLarger bool
+	var isLarger int
 	switch n := v.(type) {
 	case int64:
 		isLarger = m.isLargerThanInt(n)
 	case float64:
 		isLarger = m.isLargerThanFloat(n)
+	default:
+		return nil
 	}
-	if isLarger {
+	if isLarger > 0 || (m.exclusive && isLarger == 0) {
 		minErr := ValidationError{fmt.Sprintf("Value must be larger than %s.", m)}
 		return []ValidationError{minErr}
 	}
 	return nil
 }
 
+func (m *minimum) SetSchema(v map[string]json.RawMessage) {
+	value, ok := v["exclusiveMinimum"]
+	if ok {
+		// Ignore errors from Unmarshal. If exclusiveMinimum is a non boolean JSON
+		// value we leave it as false.
+		json.Unmarshal(value, &m.exclusive)
+	}
+}
+
 func (m *minimum) UnmarshalJSON(b []byte) error {
 	return json.Unmarshal(b, &m.Number)
 }
 
-func (m minimum) isLargerThanInt(n int64) bool {
-	if strings.Contains(m.String(), ".") {
-		flt, err := m.Float64()
-		if err != nil {
-			return false
-		}
-		if flt > float64(n) {
-			return true
-		}
-	} else {
+func (m minimum) isLargerThanInt(n int64) int {
+	if !strings.Contains(m.String(), ".") {
 		intg, err := m.Int64()
 		if err != nil {
-			return false
+			return 0
 		}
 		if intg > n {
-			return true
+			return 1
+		} else if intg < n {
+			return -1
+		}
+	} else {
+		flt, err := m.Float64()
+		if err != nil {
+			return 0
+		}
+		if flt > float64(n) {
+			return 1
+		} else if flt < float64(n) {
+			return -1
 		}
 	}
-	return false
+	return 0
 }
 
-func (m minimum) isLargerThanFloat(n float64) bool {
+func (m minimum) isLargerThanFloat(n float64) int {
 	flt, err := m.Float64()
 	if err != nil {
-		return false
+		return 0
 	}
 	if flt > n {
-		return true
+		return 1
+	} else if flt < n {
+		return -1
 	}
-	return false
+	return 0
 }
